7b2a3102c88ad725fb531af06f69a5de2ef56f03
Shlomo Starik <7696512@gmail.com>
2016-12-19 15:40:37 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/FinalProject.iml
    #	.idea/misc.xml
    #	.idea/workspace.xml
    #	gabay

--- donation_sh.py
+++ donation_sh.py
@@ -14,7 +14,7 @@ def add_donation(worshiper,donationdate,donation):
     else:
         raise ValueError("Id not exist in the database")
 
-print(add_donation(worshiper=226, donationdate=datetime(day=5,month=6,year=1987), donation='100'))
+# add_donation(worshiper=230, donationdate=datetime(day=5,month=6,year=1987), donation='100')
 
 def delete_donation(worshiper):
     examination = sqlite3.connect('gabay')
@@ -26,6 +26,36 @@ def delete_donation(worshiper):
         examination.cursor()
         examination.commit()
 
+def update_donation(worshiper, donationdate=None, donation=None):
+    # donationdate = datetime.date(donationdate)
+    examination = sqlite3.connect('gabay')
+    l = []
+    for i in examination.execute('SELECT worshiper FROM donations'):
+        l.append(i[0])
+    if str(worshiper) in l:
+        if donationdate:
+            examination.execute("UPDATE donations SET donationdate = '{}' WHERE  worshipers.id = {};".format(donationdate, worshiper))
+        if donation:
+            examination.execute("UPDATE donations SET donation = '{}' WHERE  worshipers.id = {};".format(donation, worshiper))
+        examination.cursor()
+        examination.commit()
+    else:
+        raise ValueError("Id not exist in the database")
 
-
-# delete_donation(225)
+def update_donation1(id, worshiper=None, donationdate=None, donation=None):
+    # donationdate = datetime.date(donationdate)
+    examination = sqlite3.connect('gabay')
+    l = []
+    for i in examination.execute('SELECT id FROM donations'):
+        l.append(i[0])
+    if id in l:
+        if donationdate:
+            examination.execute("UPDATE donations SET donationdate = '{}' WHERE  id = {};".format(donationdate, id))
+        if donation:
+            examination.execute("UPDATE donations SET donation = '{}' WHERE  id = {};".format(donation, id))
+        examination.cursor()
+        examination.commit()
+    else:
+        raise ValueError("Id not exist in the database")
+# delete_donation(230)
+update_donation(worshiper=229, donation='200')
