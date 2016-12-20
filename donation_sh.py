e99a8b58d03597eb567030005f602f8b50b81a8f
shlomo <shlomo@frumline.com>
2016-12-20 15:11:35 +0200

    aaa

--- donation_sh.py
+++ donation_sh.py
@@ -14,7 +14,6 @@ def add_donation(worshiper,donationdate,donation):
     else:
         raise ValueError("Id not exist in the database")
 
-# add_donation(worshiper=230, donationdate=datetime(day=5,month=6,year=1987), donation='100')
 
 def delete_donation(worshiper):
     examination = sqlite3.connect('gabay')
@@ -26,29 +25,23 @@ def delete_donation(worshiper):
         examination.cursor()
         examination.commit()
 
-def update_donation(worshiper, donationdate=None, donation=None):
-    # donationdate = datetime.date(donationdate)
-    examination = sqlite3.connect('gabay')
-    l = []
-    for i in examination.execute('SELECT worshiper FROM donations'):
-        l.append(i[0])
-    if str(worshiper) in l:
-        if donationdate:
-            examination.execute("UPDATE donations SET donationdate = '{}' WHERE  worshipers.id = {};".format(donationdate, worshiper))
-        if donation:
-            examination.execute("UPDATE donations SET donation = '{}' WHERE  worshipers.id = {};".format(donation, worshiper))
-        examination.cursor()
-        examination.commit()
-    else:
-        raise ValueError("Id not exist in the database")
 
-def update_donation1(id, worshiper=None, donationdate=None, donation=None):
-    # donationdate = datetime.date(donationdate)
+def update_donation(id, worshiper=None, donationdate=None, donation=None):
+    if donationdate:
+        donationdate = datetime.date(donationdate)
     examination = sqlite3.connect('gabay')
     l = []
     for i in examination.execute('SELECT id FROM donations'):
         l.append(i[0])
     if id in l:
+        if worshiper:
+            l = []
+            for i in examination.execute('SELECT id FROM worshipers'):
+                l.append(i[0])
+            if worshiper in l:
+                examination.execute("UPDATE donations SET worshiper = '{}' WHERE  id = {};".format(worshiper, id))
+            else:
+                raise ValueError("Id not exist in the database")
         if donationdate:
             examination.execute("UPDATE donations SET donationdate = '{}' WHERE  id = {};".format(donationdate, id))
         if donation:
@@ -57,5 +50,3 @@ def update_donation1(id, worshiper=None, donationdate=None, donation=None):
         examination.commit()
     else:
         raise ValueError("Id not exist in the database")
-# delete_donation(230)
-update_donation(worshiper=229, donation='200')
